211daf00dd23c719cc412eed105085ae85f4a011
Allen Hill <allenofthehills@gmail.com>
2019-07-23 14:44:33 -0400

    Files generated by PkgTemplates

---
+++ src/ActiveSwingAsymmetricWalkingIncreaseTrunkKinematicVariability.jl
@@ -0,0 +1,5 @@
+module ActiveSwingAsymmetricWalkingIncreaseTrunkKinematicVariability
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ActiveSwingAsymmetricWalkingIncreaseTrunkKinematicVariability
+using Test
+
+@testset "ActiveSwingAsymmetricWalkingIncreaseTrunkKinematicVariability.jl" begin
+    # Write your own tests here.
+end
